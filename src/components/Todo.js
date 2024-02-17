3ac4a88feb1cda6e32c5b30ad4103776af2dfec8
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-02-18 00:26:41 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const Todo = ({ todos, handleComplete }) => {
+  return (
+    <ul>
+      <h2>Child Component</h2>
+      {todos.map((item, index) => (
+        <li key={index}>
+          {item.title}
+          {item.visisble && (
+            <button onClick={() => handleComplete(index)}>Complete</button>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todo;
