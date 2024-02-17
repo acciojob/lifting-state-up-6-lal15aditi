50156166a6bd085a14db04e57d878281012da282
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-02-18 00:24:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,32 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
-
-const Child = ({todos, setTodos})=>{
-  function markAsComplete(i){
-    let newTodos = [...todos]
-    newTodos[i].completeStatus = true
-    setTodos(newTodos)
-  }
-  return (
-    <div className="child">
-      <h2>Child Component</h2>
-        <ul>
-          {
-            todos.map((el,i)=>{
-              return <li>{el.todo} {!el.completeStatus && <button onClick={()=>markAsComplete(i)}>Complete</button>}</li>
-            })
-          }
-        </ul>
-    </div>
-  )
-}
-
+import "./../styles/App.css";
+import Todo from "./Todo";
 
 const App = () => {
   const [todos, setTodos] = useState([
     {
-      todo: "Learn React",
-      completeStatus : false
+      title: "Learn React",
+      visisble: true,
     },
     {
-      todo: "Create a React App",
-      completeStatus : false
+      title: "Build a React app",
+      visisble: true,
     },
     {
-      todo: "Deploy the React App",
-      completeStatus : false
+      title: "Deploye the React app",
+      visisble: true,
     },
-  ])
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        <div className="parent">
-          <h1>Parent Component</h1>
-          <Child todos={todos} setTodos={setTodos} />
-        </div>
-    </div>
-  )
-}
+  ]);
 
-export default App
+  function handleComplete(index) {
+    setTodos([...todos], (todos[index].visisble = false));
+  }
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <Todo todos={todos} handleComplete={handleComplete} />
+    </div>
+  );
+};
+
+export default App;
